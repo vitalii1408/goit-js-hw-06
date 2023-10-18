bfa92e1fe142b34b326e39bb25264e822b688d31
vitalii1408 <135379297+vitalii1408@users.noreply.github.com>
2023-10-18 18:51:43 +0300

    10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,30 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const input = document.querySelector('input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxesDiv = document.querySelector('#boxes');
+function createBoxes(amount) {
+  const boxes = [];
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    boxes.push(box);
+    size += 10;
+  }
+  boxesDiv.append(...boxes);
+}
+function destroyBoxes() {
+  boxesDiv.innerHTML = '';
+}
+createButton.addEventListener('click', () => {
+  const amount = input.value;
+  createBoxes(amount);
+  input.value = '';
+});
+destroyButton.addEventListener('click', destroyBoxes);
+
