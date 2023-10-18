4fdc1722224ae7a611fe88fb4233b537182c2001
vitalii1408 <135379297+vitalii1408@users.noreply.github.com>
2023-10-18 18:32:25 +0300

    8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault(); // Забороняє перезавантаження сторінки при відправці форми
+  const formData = new FormData(loginForm);
+
+  const formDataObject = {};
+  formData.forEach((value, key) => {
+    formDataObject[key] = value;
+  });
+
+  if (!formDataObject.email || !formDataObject.password) {
+    alert("Please fill in all fields");
+    return;
+  }
+
+  console.log(formDataObject); // Виведення об'єкту в консоль
+  loginForm.reset(); // Очистити значення полів форми
+});
 
