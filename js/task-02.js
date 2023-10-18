477a88d80ddf3c773070b028d64b3942bb5a78b0
vitalii1408 <135379297+vitalii1408@users.noreply.github.com>
2023-10-18 17:27:49 +0300

    2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulElement = document.getElementById("ingredients"); 
+ingredients.forEach(ingredient => {
+  const liElement = document.createElement("li"); 
+  liElement.textContent = ingredient;
+  liElement.classList.add("item"); 
+  ulElement.appendChild(liElement);
+});
+
