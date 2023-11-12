7569101cec83b0e40caf0439a6cab649feb71c0f
vitalii1408 <135379297+vitalii1408@users.noreply.github.com>
2023-11-12 10:04:22 +0200

    2

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,5 @@
 const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
-
-nameInput.addEventListener("input", () => {
-  nameOutput.textContent = nameInput.value || "Anonymous";
+  nameInput.addEventListener('input', function() {
+  nameOutput.textContent = nameInput.value.trim() || "Anonymous";
 });
